f1a2004de662b1384eef4c0931b7929f5989a681
lingcraft <lingcraft@qq.com>
2026-06-28 17:27:17 +0800

    修复：修复增加pypinyin库之后，打包后的单文件运行无反应的问题

--- build.ps1
+++ build.ps1
@@ -1,19 +1,18 @@
 $version = (Get-Content -Path "version.json" -Raw | ConvertFrom-Json).version
 Write-Output "Yes" | nuitka mole.py `
---standalone --windows-console-mode=disable --jobs=8 --lto=yes --remove-output `
---enable-plugin=pyside6 `
---windows-icon-from-ico=icon.ico `
+--standalone --jobs=8 --lto=yes --remove-output --windows-console-mode=disable --windows-icon-from-ico=icon.ico --enable-plugin=pyside6 `
+--copyright="Copyright (C) 2025 lingcraft. All Rights Reserved" `
+--file-description=摩尔抓包工具 `
+--product-name=摩尔抓包工具 `
+--file-version=$version `
+--product-version=$version `
+--output-dir=D:\Downloads `
+--output-filename=mole.exe `
+--include-package-data=pypinyin `
 --include-data-files=hook.dll=hook.dll `
 --include-data-files=version.json=version.json `
 --include-data-files=github.ico=github.ico `
 --include-data-files=Flash.ocx=Flash.ocx `
 --include-data-files=manifest=manifest `
 --include-data-files=zh_CN.qm=zh_CN.qm `
---file-description=摩尔抓包工具 `
---product-name=摩尔抓包工具 `
---copyright="Copyright (C) 2025 lingcraft. All Rights Reserved" `
---file-version=$version `
---product-version=$version `
---onefile `
---output-filename=mole.exe `
---output-dir=D:\Downloads
+--onefile
