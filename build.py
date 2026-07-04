1f8b08eb82a5abb2275ce244a48e08c162ca38dc
lingcraft <lingcraft@qq.com>
2026-07-04 10:10:00 +0800

    优化：工作流编译流程优化，取消使用PowerShell脚本直接改用Python脚本

--- build.py
+++ build.py
@@ -1,15 +1,15 @@
 from subprocess import run
 from tomllib import load
 
-with open("pyproject.toml", "rb") as f:
-    version = load(f)["project"]["version"]
+with open("pyproject.toml", "rb") as file:
+    version = load(file)["project"]["version"]
 
-run([
+cmd = " ".join([
     "nuitka", "mole.py",
     "--standalone", "--jobs=8", "--lto=yes", "--remove-output",
     "--windows-console-mode=disable", "--windows-icon-from-ico=icon.ico",
     "--enable-plugin=pyside6",
-    '--copyright=Copyright (C) 2025 lingcraft. All Rights Reserved',
+    "--copyright=\"Copyright (C) 2025 lingcraft. All Rights Reserved\"",
     "--file-description=摩尔抓包工具",
     "--product-name=摩尔抓包工具",
     f"--file-version={version}",
@@ -24,4 +24,5 @@ run([
     "--include-data-files=manifest=manifest",
     "--include-data-files=zh_CN.qm=zh_CN.qm",
     "--onefile"
-], input=b"Yes\n")
+])
+run(cmd, input=b"Yes\n", shell=True)
