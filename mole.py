848d71b013abf89f85f872b15b276576a4aa00c9
lingcraft <lingcraft@qq.com>
2026-09-20 09:49:20 +0800

    - 优化：本地 swf 资源请求也采用分块流式转发和压缩，模仿官服资源请求过程，块大小设为 4KB，使 swf 加载进度更平滑 - 修复：修复元素骑士竞技场任务当账号掉线之后再执行失败时，重新启动任务倒计时会卡在 00:00 的问题

--- mole.py
+++ mole.py
@@ -2018,7 +2018,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             alert_msg("已完成元素骑士竞技场挑战和天赋领悟")
 
     def ysqs_arena_stop(self):
+        global is_arena_run, is_arena_choose
         if is_running("元素骑士"):
+            is_arena_run = False
+            is_arena_choose = False
             self.ysqsArenaFightButton.setText(self.ysqs_button_text)
             self.stop_update_title("元素骑士")
             self.stop_timer("元素骑士")
